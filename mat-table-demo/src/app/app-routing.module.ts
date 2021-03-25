d06b1f5e90cf10e0d67ccd1916456632519a347a
Pedro Raudry <praudry@gmail.com>
2021-03-24 21:46:07 -0500

    Adding components and types

--- mat-table-demo/src/app/app-routing.module.ts
+++ mat-table-demo/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableContainerComponent } from './components/table/table-container/table-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'apps/table',
+    component: TableContainerComponent
+  },
+  {
+    path: '',
+    redirectTo: '/app/table',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mat-table-demo/src/app/app.module.ts
+++ mat-table-demo/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MTableComponent } from './components/table/m-table/m-table.component';
+import { TableContainerComponent } from './components/table/table-container/table-container.component';
+import { AgeColumnComponent } from './components/table/columns/age-column/age-column.component';
+import { EmailColumnComponent } from './components/table/columns/email-column/email-column.component';
+import { NameColumnComponent } from './components/table/columns/name-column/name-column.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MTableComponent,
+    TableContainerComponent,
+    AgeColumnComponent,
+    EmailColumnComponent,
+    NameColumnComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
